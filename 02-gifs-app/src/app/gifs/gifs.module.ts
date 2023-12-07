ae7b5dd4eee9f36ab539231f3004b15b1cd9ac30
Siomermin <fermingonzalez011@gmail.com>
2023-12-07 19:07:54 -0300

    Fin sección 7

---
+++ 02-gifs-app/src/app/gifs/gifs.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [
+    HomePageComponent
+  ]
+})
+export class GifsModule { }
