88bca89e9eb86e0c926f7d3f4000dbcdf40cf807
Siomermin <fermingonzalez011@gmail.com>
2023-12-13 16:44:01 -0300

    Fin sección 10

---
+++ 04-pipesApp/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { PrimeNGConfig } from "primeng/api";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  constructor( private primengConfig: PrimeNGConfig ) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.primengConfig.ripple = true;
+  }
+
+}
