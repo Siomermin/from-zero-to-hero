f0731447bf075521792e003a1ccfc1fe3f4e3932
Siomermin <fermingonzalez011@gmail.com>
2023-12-15 10:33:48 -0300

    Fin Sección 12 - Pipes personalizados

--- 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
-import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
+import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { PanelModule } from 'primeng/panel';
     CardModule,
     FieldsetModule,
     MenubarModule,
-    PanelModule
+    PanelModule,
+    TableModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
