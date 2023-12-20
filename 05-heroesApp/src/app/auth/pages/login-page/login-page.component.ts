4d88b73cde37605dbe14e224b5b4aec0454c45b1
Siomermin <fermingonzalez011@gmail.com>
2023-12-20 14:25:27 -0300

    Fin Sección 16 - Guards

--- 05-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
+++ 05-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginPageComponent {
+  public user?: User;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin(): void {
+    this.authService.login('fermin@gmail.com', '123321').subscribe((user) => {
+      this.router.navigate(['/']);
+    });
+  }
 }
